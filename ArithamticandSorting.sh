4d9d1b155ae6c6fbae0ab39e90e588ccb60070e8
root <root@localhost.localdomain>
2020-03-22 16:42:04 +0000

    First Commit

--- ArithamticandSorting.sh
+++ ArithamticandSorting.sh
@@ -49,3 +49,24 @@ for ((i=0; i<4; i++))
 do
         echo ${arr[i]}
 done
+
+#Usecase 9
+for ((i=0 ; i<4 ; i++))
+do
+	
+	for ((j=0 ; j<4-i-1 ; j++))
+	do
+		if ((${arr[j]} > ${arr[$((j+1))]} ))
+		then
+			temp=${arr[j]}
+			arr[$j]=${arr[$((j+1))]}
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+
+echo "Ascending Array:"
+for ((i=0; i<4; i++))
+do
+	echo ${arr[i]}
+done
